bc58b3bdf28cffd4cc392aeaef9c58ab356bdef4
Joshua Marcum <josh.marcum@icloud.com>
2025-10-11 20:00:58 -0700

    automated portfolio push of current sources.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -24,9 +24,8 @@ export const Footer = () => {
       >
         <Text variant="body-default-s" onBackground="neutral-strong">
           <Text onBackground="neutral-weak">© {currentYear} /</Text>
-          <Text paddingX="4">Portfolio and blog of {person.name}</Text>
+          <Text paddingX="4">securityengineerd.com</Text>
           <Text onBackground="neutral-weak">
-            {/* Usage of this template requires attribution. Please don't remove the link to Once UI. */}
             / {" "}
             <SmartLink
               href="https://once-ui.com/products/magic-portfolio"
